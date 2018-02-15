9a0f51d11ed3d202a9a4ed6179cebbade3d216df
Luke Shiru <hi@luke.sh>
2018-02-15 13:42:25 -0300

    v1.0.0 - Checout CHANGELOG.md

---
+++ src/windowOpenPromise.ts
@@ -0,0 +1,24 @@
+import { WindowOpenError } from "./WindowOpenError";
+import { WindowOpenPromiseFunction } from "./WindowOpenPromiseFunction";
+
+/**
+ * Promised Window.open.
+ * @param url Target URL.
+ * @param target Target attribute (_blank, _parent, _self, _top) or name.
+ * @param features New window features.
+ * @param replace New entry or replace in browser history.
+ */
+export const windowOpenPromise: WindowOpenPromiseFunction = async (
+	url?,
+	target?,
+	features?,
+	replace?
+) =>
+	new Promise<Window>((resolve, reject) => {
+		const newWindow = window.open(url, target, features, replace);
+		if (newWindow !== null) {
+			resolve(newWindow);
+		} else {
+			reject(new WindowOpenError());
+		}
+	});
