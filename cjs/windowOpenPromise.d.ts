d72d7bae6725cfe16c29037b6419dc13637e8f64
Lucas Ciruzzi <hello@lukeshiru.me>
2019-04-02 16:11:22 -0300

    v2.0.1 - Checkout CHANGELOG.md

--- cjs/windowOpenPromise.d.ts
+++ cjs/windowOpenPromise.d.ts
@@ -1,9 +1,6 @@
 import { WindowOpenPromiseFunction } from "./WindowOpenPromiseFunction";
 /**
  * Promised Window.open.
- * @param url Target URL.
- * @param target Target attribute (_blank, _parent, _self, _top) or name.
- * @param features New window features.
- * @param replace New entry or replace in browser history.
+ * @param options WindowOpenPromise options.
  */
 export declare const windowOpenPromise: WindowOpenPromiseFunction;
