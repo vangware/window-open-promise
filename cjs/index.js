886111385a2a19eca241769ef1e87e4e7db0d5dc
Luke Shiru <hi@luke.sh>
2019-06-24 02:42:43 -0300

    v2.0.4 - Checkout CHANGELOG.md.

--- cjs/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var windowOpenPromise_1 = require("./windowOpenPromise");
-exports.windowOpenPromise = windowOpenPromise_1.windowOpenPromise;
--- esm/index.d.ts
+++
@@ -1,1 +0,0 @@
-export { windowOpenPromise } from "./windowOpenPromise";
