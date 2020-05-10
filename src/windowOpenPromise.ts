d2e3f0dd90072bc96851efb9a9a7bb11d54fbeef
Luke Shiru <hi@luke.sh>
2020-05-10 01:06:08 -0700

    v2.5.1 - Checkout CHANGELOG.md

--- src/windowOpenPromise.ts
+++ src/windowOpenPromise.ts
@@ -1,4 +1,4 @@
-import { EMPTY_OBJECT, EMPTY_STRING, when } from "@vangware/micro";
+import { isNull } from "@vangware/micro";
 import featureParser from "./featureParser";
 import type WindowOpenPromiseFunction from "./WindowOpenPromiseFunction";
 
@@ -7,17 +7,22 @@ import type WindowOpenPromiseFunction from "./WindowOpenPromiseFunction";
  * @param options - WindowOpenPromise options.
  */
 export const windowOpenPromise: WindowOpenPromiseFunction = ({
-	url = EMPTY_STRING,
-	target = EMPTY_STRING,
-	features = EMPTY_OBJECT,
+	url = "",
+	target = "",
+	features = {},
 	replace = false
 }) =>
-	new Promise<Window>((resolve, reject) =>
-		when(
-			window.open(url, target, featureParser(features), replace),
-			resolve,
-			reject
-		)
-	);
+	new Promise<Window>((resolve, reject) => {
+		const newWindow = window.open(
+			url,
+			target,
+			featureParser(features),
+			replace
+		);
+
+		return isNull(newWindow)
+			? reject(new Error("Window can't be opened"))
+			: resolve(newWindow);
+	});
 
 export default windowOpenPromise;
