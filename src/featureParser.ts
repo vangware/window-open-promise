d2eb294c8024138ad65180c9fa4bda03a08ec8cb
Luke Shiru <hello@lukeshiru.me>
2020-05-20 19:04:28 -0700

    v2.5.4 - Checkout CHANGELOG.md.

--- src/featureParser.ts
+++ src/featureParser.ts
@@ -1,18 +1,29 @@
 import { arrayMap, Entry, isBoolean, objectEntries } from "@vangware/micro";
+import {
+	FEATURE_DISABLED,
+	FEATURE_ENABLED,
+	FEATURE_SEPARATOR
+} from "./constants";
 import type WindowOpenPromiseFeatures from "./WindowOpenPromiseFeatures";
 
 /**
  * Parses features object into features string.
  * @param features Features object.
  */
-export const featureParser = (features?: WindowOpenPromiseFeatures) =>
-	features !== undefined
-		? arrayMap<Entry<WindowOpenPromiseFeatures>, string>(
-				([feature, value]) =>
-					`${feature}=${
-						isBoolean(value) ? (value ? "1" : "0") : value
-					}`
-		  )(objectEntries(features)).join(",")
-		: undefined;
+export const featureParser = (features: WindowOpenPromiseFeatures = {}) => {
+	const featureMap = arrayMap<Entry<WindowOpenPromiseFeatures>, string>(
+		([feature, value]) =>
+			`${feature}=${
+				isBoolean(value)
+					? value
+						? FEATURE_ENABLED
+						: FEATURE_DISABLED
+					: value
+			}`
+	);
+	const featureEntries = objectEntries(features);
+
+	return featureMap(featureEntries).join(FEATURE_SEPARATOR);
+};
 
 export default featureParser;
