80bc7bfedd622d6d0d093e7dddedf5b3b5fe6f1e
Luke Shiru <hi@luke.sh>
2020-06-19 20:31:13 -0700

    v2.6.5 - Checkout CHANGELOG.md.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { featureParser } from "./featureParser";
 export { default, windowOpenPromise } from "./windowOpenPromise";
-export type { WindowOpenPromiseFeatures } from "./WindowOpenPromiseFeatures";
-export type { WindowOpenPromiseFunction } from "./WindowOpenPromiseFunction";
-export type { WindowOpenPromiseOptions } from "./WindowOpenPromiseOptions";
+export { WindowOpenPromiseFeatures } from "./WindowOpenPromiseFeatures";
+export { WindowOpenPromiseFunction } from "./WindowOpenPromiseFunction";
+export { WindowOpenPromiseOptions } from "./WindowOpenPromiseOptions";
