88e63b5fccd11b19d6015a87327f43feda2a6fff
Luke Shiru <hi@lshi.ru>
2020-10-21 19:22:01 -0700

    v4.0.0 - Checkout CHANGELOG.md.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export { featureParser } from "./featureParser";
-export { default, windowOpenPromise } from "./windowOpenPromise";
+export { windowOpenPromise } from "./windowOpenPromise";
 export { WindowOpenPromiseFeatures } from "./WindowOpenPromiseFeatures";
-export { WindowOpenPromiseFunction } from "./WindowOpenPromiseFunction";
 export { WindowOpenPromiseOptions } from "./WindowOpenPromiseOptions";
