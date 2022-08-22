e97b94251b29be14c3216555c227cd6c9fb4536b
Lou Cyx <git@lou.cx>
2022-08-22 01:50:44 -0700

    :arrow_up: dependency upgrade.

--- tests/windowOpenPromise.test.ts
+++ tests/windowOpenPromise.test.ts
@@ -20,33 +20,37 @@ export default [
 	{
 		given: "an object with options",
 		must: "return mock with the same options",
-		received: windowOpen({
-			height: 100,
-			left: 100,
-			menuBar: true,
-			noOpener: true,
-			scrollbars: false,
-			target: "target",
-			url: "url",
-		}).catch(_ => "Error"),
-		wanted: {
-			features: "height=100,left=100,menubar=1,noopener=1,scrollbars=0",
-			target: "target",
-			url: "url",
-		} as unknown as Window,
+		received: () =>
+			windowOpen({
+				height: 100,
+				left: 100,
+				menuBar: true,
+				noOpener: true,
+				scrollbars: false,
+				target: "target",
+				url: "url",
+			}).catch(_ => "Error"),
+		wanted: () =>
+			({
+				features:
+					"height=100,left=100,menubar=1,noopener=1,scrollbars=0",
+				target: "target",
+				url: "url",
+			} as unknown as Window),
 	},
 	{
 		given: "an object with options and a blocked window",
 		must: "return the error to be cached",
-		received: windowOpenFail({
-			height: 100,
-			left: 100,
-			menuBar: true,
-			noOpener: true,
-			scrollbars: false,
-			target: "target",
-			url: "url",
-		}).catch(_ => "Error"),
-		wanted: "Error" as unknown as Window,
+		received: () =>
+			windowOpenFail({
+				height: 100,
+				left: 100,
+				menuBar: true,
+				noOpener: true,
+				scrollbars: false,
+				target: "target",
+				url: "url",
+			}).catch(_ => "Error"),
+		wanted: () => "Error" as unknown as Window,
 	},
 ] as Tests<Window>;
